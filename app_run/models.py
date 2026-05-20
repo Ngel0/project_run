6f273a3c006a6eff41d075534058b5e62b64b0bd
Oleg Pak <pak.o99@mail.ru>
2026-05-20 15:39:03 +0300

    changed User to settings.AUTH_USER_MODEL

--- app_run/models.py
+++ app_run/models.py
@@ -1,7 +1,7 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.conf import settings
 
 class Run(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
-    athlete = models.ForeignKey(User, on_delete=models.CASCADE)
+    athlete = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     comment = models.TextField()
