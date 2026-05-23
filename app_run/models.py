a43889cceb2ff6c1c80c0f5e0990a1d1e43d6a49
Oleg Pak <pak.o99@mail.ru>
2026-05-23 14:59:24 +0300

    added status field to run model

--- app_run/models.py
+++ app_run/models.py
@@ -5,3 +5,9 @@ class Run(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     athlete = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     comment = models.TextField()
+    STATUS_CHOICES = [
+        ('init', 'Забег инициализирован'),
+        ('in_progress', 'Забег начат'),
+        ('finished', 'Забег закончен')
+    ]
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='init')
